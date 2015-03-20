861eeb223a3264f213214d6cab580701d2622f61
bontaq <bontaq@gmail.com>
2015-03-20 19:54:10 -0400

    Styling changes -- move GPS button to over the stops search.

--- priv/static/js/script.js
+++ priv/static/js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
       busName = "";
 
   if(navigator.geolocation) {
-    $('#gps-button').show();
+    $('#gps-locator').show();
   }
 
   $('#search').keyup(function(){
